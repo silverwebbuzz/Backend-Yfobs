2b2e8303d84fbb0b23dacbcbb456e06e86143b81
hirenlodha07 <hiren@silverwebbuzz@gmail.com>
2022-12-28 13:24:09 +0530

    update cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  app.enableCors();
   await app.listen(8001);
 }
 bootstrap();
